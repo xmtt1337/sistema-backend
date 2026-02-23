1c1a89c3c242bacea0b840237531e2162c104086
xmtt1337 <cobrador10rs@gmail.com>
2026-02-23 16:18:30 -0300

    config porta render

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.post("/login", (req, res) => {
     }
 });
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Servidor rodando na porta " + PORT);
 });
